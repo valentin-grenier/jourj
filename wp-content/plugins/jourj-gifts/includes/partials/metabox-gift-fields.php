f6f45ce8d7651e9e46d7ccbf3b04241ffacd36d2
Valentin Grenier <valentin.grenier02@gmail.com>
2025-04-21 09:49:00 +0200

    feat: Added meta boxes disabled attribute to prevent users editing core infos, edited inputs order

--- wp-content/plugins/jourj-gifts/includes/partials/metabox-gift-fields.php
+++ wp-content/plugins/jourj-gifts/includes/partials/metabox-gift-fields.php
@@ -35,40 +35,6 @@
             style="width: 100%; max-width: 500px;"><?php echo esc_textarea($custom_message); ?></textarea>
     </p>
 
-    <!-- Reserved (checkbox) -->
-    <p>
-        <input
-            type="checkbox"
-            name="jourj_reserved"
-            id="jourj_reserved"
-            value="1"
-            <?php checked($reserved, 1); ?>>
-        <label for="jourj_reserved"><?php esc_html_e('Reserved?', 'jourj'); ?></label>
-    </p>
-
-    <!-- Already Funded -->
-    <p>
-        <label for="jourj_funded"><?php esc_html_e('Already Funded Amount:', 'jourj'); ?></label><br>
-        <input
-            type="number"
-            step="0.01"
-            name="jourj_funded"
-            id="jourj_funded"
-            value="<?php echo esc_attr($funded); ?>"
-            style="width: 100%; max-width: 300px;">
-    </p>
-
-    <!-- Reserved By Email -->
-    <p>
-        <label for="jourj_reserved_by"><?php esc_html_e('Reserved By (Email):', 'jourj'); ?></label><br>
-        <input
-            type="email"
-            name="jourj_reserved_by"
-            id="jourj_reserved_by"
-            value="<?php echo esc_attr($reserved_by); ?>"
-            style="width: 100%; max-width: 300px;">
-    </p>
-
     <!-- is_featured (checkbox) -->
     <p>
         <input
@@ -81,4 +47,46 @@
             <?php esc_html_e('Highlight this gift?', 'jourj'); ?>
         </label>
     </p>
+
+    <!-- Separator -->
+    <hr>
+
+    <!-- Reserved (checkbox) -->
+    <p>
+        <input
+            type="checkbox"
+            name="jourj_reserved"
+            id="jourj_reserved"
+            value="1"
+            disabled
+            <?php checked($reserved, 1); ?>>
+        <label for="jourj_reserved"><?php esc_html_e('Reserved?', 'jourj'); ?></label>
+    </p>
+
+    <!-- Already Funded -->
+    <p>
+        <label for="jourj_funded"><?php esc_html_e('Already Funded Amount:', 'jourj'); ?></label><br>
+        <input
+            type="number"
+            step="0.01"
+            name="jourj_funded"
+            id="jourj_funded"
+            disabled
+            value="<?php echo esc_attr($funded); ?>"
+            style="width: 100%; max-width: 300px;">
+    </p>
+
+    <!-- Reserved By Email -->
+    <p>
+        <label for="jourj_reserved_by"><?php esc_html_e('Reserved By (Email):', 'jourj'); ?></label><br>
+        <input
+            type="email"
+            name="jourj_reserved_by"
+            id="jourj_reserved_by"
+            value="<?php echo esc_attr($reserved_by); ?>"
+            style="width: 100%; max-width: 300px;"
+            disabled>
+    </p>
+
+
 </div>
