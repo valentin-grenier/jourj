b8d405fb49b3e857f7efa8568704573ba0ed2962
Valentin Grenier <valentin.grenier02@gmail.com>
2025-04-22 08:55:24 +0200

    feat: Add .env file and variable check on plugin init

--- wp-content/plugins/jourj-gifts/includes/class-init.php
+++ wp-content/plugins/jourj-gifts/includes/class-init.php
@@ -13,6 +13,7 @@ class JourJ_Init
     {
         add_action('wp_enqueue_scripts', [$this, 'load_plugin_assets']);
         add_action('admin_enqueue_scripts', [$this, 'load_plugin_admin_assets']);
+        add_action('init', [$this, 'check_env_file']);
     }
 
     # Initialize plugin assets
@@ -39,5 +40,19 @@ class JourJ_Init
 
         # CSS
         wp_enqueue_style('jourj-gifts-admin-styles', $plugin_root_url . 'assets/css/admin.css', array(), '1.0.0', 'all');
+    }
+
+    # Check if .env and PAYPAL_EMAIL are set in root directory, else add an admin notice
+    public function check_env_file()
+    {
+        if (!file_exists(ABSPATH . '.env')) {
+            add_action('admin_notices', function () {
+                echo '<div class="notice notice-error"><p>' . __('.env file not found in the root directory. Please create one with the required variables.', 'jourj-gifts') . '</p></div>';
+            });
+        } elseif (!isset($_ENV['PAYPAL_EMAIL'])) {
+            add_action('admin_notices', function () {
+                echo '<div class="notice notice-error"><p>' . __('PAYPAL_EMAIL is not set in the .env file. Please set it to your PayPal email address.', 'jourj-gifts') . '</p></div>';
+            });
+        }
     }
 }
